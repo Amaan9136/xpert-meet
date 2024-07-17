ce33ccc114a8b9a5912a417b64621ada905b8969
Amaan MK <120761340+Amaan9136@users.noreply.github.com>
2024-07-17 22:45:30 +0530

    v 1.2 - connection to p2p provided
    Video call connection with other peers is provided in this commit

--- index.js
+++ index.js
@@ -1,54 +1,21 @@
-// get firebase ref from app context and use the same ref everywhere
-import React, { Suspense, StrictMode } from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./index.css";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { createStore } from "redux";
+import App from './App';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
 import { userReducer } from "./store/reducer";
-import AppContextProvider from "./AppContext";
-import Loader from './AppLoader';
-import reportWebVitals from "./reportWebVitals";
 
 export const store = createStore(userReducer);
-// store has all the values of db
-const routes = [
-  {
-    path: "/",
-    component: React.lazy(() => import("./App.js")),
-  },
-  {
-    path: "/summary",
-    component: React.lazy(() => import("./pages/Summary/Summary.js")),
-  },
-  {
-    path: "/internet",
-    component: React.lazy(() => import("./pages/NoInternet/NoInternet.component.js")),
-  },
-];
-
-const Root = () => (
-  <Router>
-    <Provider store={store}>
-      <AppContextProvider>
-        <Suspense fallback={<Loader />}>
-          <Routes>
-            {routes.map(({ path, component: Component }) => (
-              <Route key={path} path={path} element={<Component />} />
-            ))}
-          </Routes>
-        </Suspense>
-      </AppContextProvider>
-    </Provider>
-  </Router>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
-
-const app = ReactDOM.createRoot(document.getElementById("app"));
-app.render(
-  // <StrictMode>
-    <Root />
-  // </StrictMode> 
-);
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
