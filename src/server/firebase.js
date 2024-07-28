507dd1fdf1e70308b6d939cc74f05fea104d81c8
Amaan MK <120761340+Amaan9136@users.noreply.github.com>
2024-07-28 13:22:25 +0530

    chore: #16 Performance of Video is Enhanced

--- src/server/firebase.js
+++ src/server/firebase.js
@@ -1,20 +1,15 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/database';
 
-
 var firebaseConfig = {
-  apiKey: "AIzaSyAcvzFXEUPBpUe69uERDTpPh_zXGglmGwI", // Add API Key
-  databaseURL:"https://vivid-alchemy-399203-default-rtdb.firebaseio.com/" // Add databaseURL
+  apiKey: "AIzaSyAcvzFXEUPBpUe69uERDTpPh_zXGglmGwI",
+  databaseURL: "https://vivid-alchemy-399203-default-rtdb.firebaseio.com/",
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
 export const db = firebase;
- 
 var firepadRef = firebase.database().ref();
 
-export const userName = prompt("What's your name?");
 const urlparams = new URLSearchParams(window.location.search);
 const roomId = urlparams.get("id");
 
@@ -22,7 +17,6 @@ if (roomId) {
   firepadRef = firepadRef.child(roomId);
 } else {
   firepadRef = firepadRef.push();
-  window.history.replaceState(null, "Meet", "?id=" + firepadRef.key);
 }
 
 export default firepadRef;
