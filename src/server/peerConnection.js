89c5c907b16573f556d59b04bf0703c400a81358
Amaan MK <120761340+Amaan9136@users.noreply.github.com>
2024-07-28 14:15:50 +0530

    fix: #22 Multi-media Enhancement

--- src/server/peerConnection.js
+++ src/server/peerConnection.js
@@ -1,100 +1,22 @@
-import { store } from "../index";
-import firepadRef from "./firebase";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
 
-const participantRef = firepadRef.child("participants");
-
-export const updatePreference = (userId, preference) => {
-  const currentParticipantRef = participantRef.child(userId).child("preferences");
-  setTimeout(() => {
-    currentParticipantRef.update(preference);
-  },50);
+var firebaseConfig = {
+  apiKey: "AIzaSyAcvzFXEUPBpUe69uERDTpPh_zXGglmGwI",
+  databaseURL: "https://vivid-alchemy-399203-default-rtdb.firebaseio.com/",
 };
 
-export const createOffer = (peerConnection, receiverId, createdID) => {
-  const currentParticipantRef = participantRef.child(receiverId);
+firebase.initializeApp(firebaseConfig);
+export const db = firebase;
+var firepadRef = firebase.database().ref();
 
-  peerConnection.onicecandidate = (event) => {
-    if (event.candidate) {
-      currentParticipantRef
-        .child("offerCandidates")
-        .push({ ...event.candidate.toJSON(), userId: createdID });
-    }
-  };
+const urlparams = new URLSearchParams(window.location.search);
+const roomId = urlparams.get("id");
 
-  return peerConnection.createOffer()
-    .then((offerDescription) => {
-      return peerConnection.setLocalDescription(offerDescription)
-        .then(() => {
-          const offer = {
-            sdp: offerDescription.sdp,
-            type: offerDescription.type,
-            userId: createdID,
-          };
-          return currentParticipantRef.child("offers").push().set({ offer });
-        });
-    });
-};
+if (roomId) {
+  firepadRef = firepadRef.child(roomId);
+} else {
+  firepadRef = firepadRef.push();
+}
 
-export const initializeListeners = (userId) => {
-  const currentUserRef = participantRef.child(userId);
-
-  currentUserRef.child("offers").on("child_added", (snapshot) => {
-    const data = snapshot.val();
-    if (data?.offer) {
-      const pc = store.getState().participants[data.offer.userId].peerConnection;
-      pc.setRemoteDescription(new RTCSessionDescription(data.offer))
-        .then(() => createAnswer(data.offer.userId, userId));
-    }
-  });
-
-  currentUserRef.child("offerCandidates").on("child_added", (snapshot) => {
-    const data = snapshot.val();
-    if (data.userId) {
-      const pc = store.getState().participants[data.userId].peerConnection;
-      pc.addIceCandidate(new RTCIceCandidate(data));
-    }
-  });
-
-  currentUserRef.child("answers").on("child_added", (snapshot) => {
-    const data = snapshot.val();
-    if (data?.answer) {
-      const pc = store.getState().participants[data.answer.userId].peerConnection;
-      const answerDescription = new RTCSessionDescription(data.answer);
-      pc.setRemoteDescription(answerDescription);
-    }
-  });
-
-  currentUserRef.child("answerCandidates").on("child_added", (snapshot) => {
-    const data = snapshot.val();
-    if (data.userId) {
-      const pc = store.getState().participants[data.userId].peerConnection;
-      pc.addIceCandidate(new RTCIceCandidate(data));
-    }
-  });
-};
-
-const createAnswer = (otherUserId, userId) => {
-  const pc = store.getState().participants[otherUserId].peerConnection;
-  const participantRef1 = participantRef.child(otherUserId);
-
-  pc.onicecandidate = (event) => {
-    if (event.candidate) {
-      participantRef1
-        .child("answerCandidates")
-        .push({ ...event.candidate.toJSON(), userId: userId });
-    }
-  };
-
-  return pc.createAnswer()
-    .then((answerDescription) => {
-      return pc.setLocalDescription(answerDescription)
-        .then(() => {
-          const answer = {
-            type: answerDescription.type,
-            sdp: answerDescription.sdp,
-            userId: userId,
-          };
-          return participantRef1.child("answers").push().set({ answer });
-        });
-    });
-};
+export default firepadRef;
